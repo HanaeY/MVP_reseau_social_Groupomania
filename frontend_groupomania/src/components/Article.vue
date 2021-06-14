4e1fd2690826ee53e882a93200935199e7bbe6b7
Hanae <hanae.yamashita@lilo.org>
2021-06-14 13:16:06 +0200

    structure home et composant article

---
+++ frontend_groupomania/src/components/Article.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="article">
+    <div class="content">
+      <p class="content__publication">Publié par <span>[USERNAME]</span> le <span>[DATE]</span></p>
+      <div class = "content__image">[image]</div>
+      <p class="content__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam harum, perferendis quaerat facilis omnis voluptate animi repellat at id impedit, sapiente fuga recusandae quidem aliquid autem, accusantium ullam reiciendis? Architecto.</p>
+    </div>
+    <div class="reactions">
+      <h3>Commentaires</h3>
+      <div class="reactions__add">
+        <input type="text" class="reactions__add-comment" placeholder="j'ajoute un commentaire ici ...">
+        <button type="submit" class="reactions__add-submit">publier</button>
+      </div>
+      <div class="reactions__see">
+        <h4>Dernier commentaire</h4>
+        <div>
+          <p class="content__description"><span>[USERNAME] </span><span>([DATE])</span> : <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam harum, perferendis quaerat facilis omnis voluptate animi repellat at id impedit, sapiente fuga recusandae quidem aliquid autem, accusantium ullam reiciendis? Architecto.</span></p>
+        </div>
+        <h4>Voir tout</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Article',
+  props: {
+    //msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.article {
+  width: 80%;
+  margin: auto;
+  border: 2px solid;
+  border-radius: 15px;
+  text-align: left;
+  padding: 5px;
+}
+
+.content {
+    &__image {
+    width: 500px;
+    height: 300px;
+    background-color: violet;
+    margin: auto;
+  }
+}
+
+.reactions {
+  &__add {
+    width: 100%;
+    display: flex;
+    &-comment {
+      flex: 2;
+      height: 50px;
+    }
+    &-submit {
+      height: 56px;
+      padding: 10px;
+    }
+  }
+}
+
+
+</style>
--- frontend_groupomania/src/views/Home.vue
+++ frontend_groupomania/src/views/Home.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Bienvenue [USERNAME]</h1>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <h2>Forums</h2>
+      <div id="buttons">
+        <button>Multimedia</button>
+        <button>Articles</button>
+      </div>
+    <h2>Dernières publications</h2>
+      <Article/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
+import Article from '@/components/Article.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Article
+    //HelloWorld
   }
 }
 </script>
+
+<style lang="scss">
+  #buttons {
+    display: flex;
+    justify-content: center;
+    button {
+      width: 400px;
+      height: 100px;
+      margin: 10px;
+    }
+  }
+</style>
