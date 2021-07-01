26c0c10306fbf340d52f2a9a84c753c295018333
Hanae <hanae.yamashita@lilo.org>
2021-07-01 18:20:34 +0200

    style commentaires

--- frontend_groupomania/src/components/Comment.vue
+++ frontend_groupomania/src/components/Comment.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="comment">
+  <div>
+    <div class="comment">
       <p class="comment__information"> Posté par {{ comment.User.username }} le {{ date }}</p>
-      <p class="content__content">{{ comment.comment }}</p> 
-      <button v-if="user.id == comment.UserId" @click="deleteComment">supprimer mon commentaire</button>
-      <p v-if="error">{{ error }}</p>
-      <p v-if="message">{{ message }}</p>
+      <p class="comment__content">{{ comment.comment }}</p> 
+      <button class="comment__delete" v-if="user.id == comment.UserId" @click="deleteComment">supprimer mon commentaire</button>
+    </div>  
+
+    <p v-if="error">{{ error }}</p>
+    <p v-if="message">{{ message }}</p>
   </div>
 </template>
 
@@ -44,4 +47,29 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.comment {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  &__information {
+    margin-right: 10px;
+    font-style: italic;
+    font-size: 0.9em;
+  }
+  &__delete {
+    margin-left: 10px;
+    border: 1px solid red;
+    color: red;
+    background:white;
+    border-radius: 7px;
+    height: 20px;
+    font-size: 0.7em;
+  }
+  &__content {
+    color: white;
+    background:#000033;
+    border-radius: 10px;
+    padding: 5px 10px;
+  }
+}
 </style>
